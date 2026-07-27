8b7c68210f74083b8b93db2025a33b3b7c4764be
Stefano Moia <s.moia.research@gmail.com>
2026-07-27 12:29:01 +0200

    Fix: Make laplskel executable

--- laplskel/__init__.py
+++ laplskel/__init__.py
@@ -1,5 +1,6 @@
 """Main init for crispy package."""
 
 from . import _version
+from .laplskel import _main
 
-__version__ = _version.get_versions()["version"]
+__version__ = _version.get_versions()['version']
